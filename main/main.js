b8d36d30658adb4daed49e9d203961cef13a46e9
“Dean” <467271883@qq.com>
2019-07-10 20:16:52 +0800

    Change the barCodeFormat and delete the barCodeDuplicate

--- main/main.js
+++ main/main.js
@@ -13,7 +13,7 @@ const getTotalPrice = (after_promotion_items) => {
 const getReceiptMsg = (items) => {
     let result = ""
     items.forEach(item => {
-        result += "名称：${item.name}，数量：${item.count}瓶，单价：${item.price}(元)，小计：${item.realPrice}(元)\n"
+        result += `名称：${item.name}，数量：${item.count}${item.unit}，单价：${item.price}(元)，小计：${item.realPrice}(元)\n`
     })
     return result
 }
@@ -28,7 +28,7 @@ const getPromotions = (items) => {
     let promotionBarcode = temp[0].barcodes;
     return items.map(item => {
         if(promotionBarcode.indexOf(item.barcode) !== -1){
-            item.realPrice = (item.totalPrice - parseInt(item.count / 2) * item.price).toFixed(2)
+            item.realPrice = (item.totalPrice -  item.price).toFixed(2)
         }else{
             item.realPrice = item.totalPrice
         }
@@ -51,31 +51,33 @@ const getItemsMsg = (duplicate_barcodes) => {
 }
 
 const barcodeFormat = barcodes => {
+    let result = {}
     barcodes.forEach(barcode => {
         if(barcode.indexOf('-') !== -1){
             let index = barcode.indexOf('-')
             let real_barcode = barcode.substring(0,index);
             let count = parseFloat(barcode.substring(index + 1));
-            barcodes.splice(barcodes.indexOf(barcode),1)
-            for(let i = 0;i < count;i++){
-                barcodes.push(real_barcode)
+            if(result[real_barcode] === undefined){
+                result[real_barcode] = count;
+            }else{
+                result[real_barcode] += count;
+            }
+        }else{
+            if(result[barcode] === undefined){
+                result[barcode] = 1;
+            }else{
+                result[barcode] += 1;
             }
         }
     })
-    return barcodes;
+    return result;
 }
 
-const barcodeDuplicate = barcodes => {
-    return barcodes.reduce((re, cur) => {
-        re[cur] ? re[cur]++ : re[cur] = 1;
-        return re;
-    },{})
 
-}
+
 
 const printReceipt = (barcodes) => {
-    const barcode_format_data = barcodeFormat(barcodes);
-    const barcode_duplicate_data = barcodeDuplicate(barcode_format_data)
+    const barcode_duplicate_data = barcodeFormat(barcodes)
     const get_item_msg = getItemsMsg(barcode_duplicate_data)
     const promotions_data = getPromotions(get_item_msg)
     const total_price_and_save_price = getTotalPrice(promotions_data)
@@ -83,8 +85,8 @@ const printReceipt = (barcodes) => {
     let result = "***<没钱赚商店>收据***\n"
     result += getReceiptMsg(promotions_data)
     result += "----------------------\n"
-    result += "总计：${total_price_and_save_price.real_price}(元)\n"
-    result += "节省：${total_price_and_save_price.save_price}(元)\n"
+    result += `总计：${total_price_and_save_price.real_price}(元)\n`
+    result += `节省：${total_price_and_save_price.save_price}(元)\n`
     result += "**********************";
-    return result;
+    console.log(result)
 }
